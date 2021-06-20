386353e448c77b5e7a3007b77303b2447de42df9
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-06-21 00:23:06 +0530

    combine two tabel.sql

---
+++ combine two table1.sql
@@ -0,0 +1,31 @@
+'''
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| PersonId    | int     |
+| FirstName   | varchar |
+| LastName    | varchar |
++-------------+---------+
+PersonId is the primary key column for this table.
+Table: Address
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| AddressId   | int     |
+| PersonId    | int     |
+| City        | varchar |
+| State       | varchar |
++-------------+---------+
+AddressId is the primary key column for this table.
+ 
+
+Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of those people:
+
+FirstName, LastName, City, State
+
+'''
+# Write your MySQL query statement below
+select Firstname,Lastname,City,State from Person left join Address using(PersonId);

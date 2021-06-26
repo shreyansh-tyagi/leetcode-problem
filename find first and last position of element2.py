5f786203c901d9b43b3d6883ef47350d5f4c2e5e
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-06-26 20:41:38 +0530

    first missing positive

---
+++ find first and last position of element2.py
@@ -0,0 +1,37 @@
+''''
+Given an unsorted integer array nums, find the smallest missing positive integer.
+
+You must implement an algorithm that runs in O(n) time and uses constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,0]
+Output: 3
+Example 2:
+
+Input: nums = [3,4,-1,1]
+Output: 2
+Example 3:
+
+Input: nums = [7,8,9,11,12]
+Output: 1
+ 
+
+Constraints:
+
+1 <= nums.length <= 5 * 105
+-231 <= nums[i] <= 231 - 1
+
+'''
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums.sort()
+        a=1
+        for i in range(len(nums)):
+            if nums[i]==a:
+                a+=1
+            
+        else:
+            return a    

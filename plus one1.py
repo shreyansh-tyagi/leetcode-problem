019ab53ec09b77810f0d9d26df379a752f045eb7
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-06-29 12:47:13 +0530

    plus add

---
+++ plus one1.py
@@ -0,0 +1,42 @@
+''''
+Given a non-empty array of decimal digits representing a non-negative integer, increment one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+ 
+
+Example 1:
+
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Example 2:
+
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Example 3:
+
+Input: digits = [0]
+Output: [1]
+ 
+
+Constraints:
+
+1 <= digits.length <= 100
+0 <= digits[i] <= 9
+''''
+class Solution:
+	def plusOne(self, digits: List[int]) -> List[int]:
+		s=0
+		d=[]
+		for i in digits:
+			s=10*s+i
+		s=s+1
+		while s!=0:
+			r=s%10
+			d.append(r)
+			s=s//10
+		return d[::-1]

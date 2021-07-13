8a9d842e0c509a430b97c06c254eb672ce43a944
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-07-13 20:25:27 +0530

    changes made from now

--- count negative number in sorted array.py
+++ count negative number in sorted array.py
@@ -7,7 +7,7 @@ Example 1:
 
 Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
 Output: 8
-Explanation: There are 8 negatives number in the matri
+Explanation: There are 8 negatives number in the matrix.
 Example 2:
 
 Input: grid = [[3,2],[1,0]]

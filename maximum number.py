e20e9e3067af9959e6de5d750ae41af18e1da752
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-07-14 23:20:14 +0530

    maximum number

---
+++ maximum number.py
@@ -0,0 +1,42 @@
+'''
+Given integer array nums, return the third maximum number in this array. If the third maximum does not exist, return the maximum number.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,1]
+Output: 1
+Explanation: The third maximum is 1.
+Example 2:
+
+Input: nums = [1,2]
+Output: 2
+Explanation: The third maximum does not exist, so the maximum (2) is returned instead.
+Example 3:
+
+Input: nums = [2,2,3,1]
+Output: 1
+Explanation: Note that the third maximum here means the third maximum distinct number.
+Both numbers with value 2 are both considered as second maximum.
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+ 
+
+Follow up: Can you find an O(n) solution?
+
+'''
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        a=list(set(nums))
+        if len(a)>=3:
+            for i in range(2):
+                b=max(a)
+                a.remove(b)
+            return max(a)    
+        else:
+            return max(a)

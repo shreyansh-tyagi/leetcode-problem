6c4c8a581df2f54a194fb229ad1b9ef093f064c6
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-07-19 19:01:32 +0530

    rising temperature

---
+++ rising temperature.sql
@@ -0,0 +1,47 @@
+'''
+SQL Schema
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature in a certain day.
+ 
+
+Write an SQL query to find all dates' id with higher temperature compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The query result format is in the following example:
+
+Weather
++----+------------+-------------+
+| id | recordDate | Temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+
+Result table:
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+In 2015-01-02, temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, temperature was higher than the previous day (20 -> 30).
+
+'''
+
+select w1.id
+from Weather w1 join Weather w2 on DATEDIFF(w1.recordDate, w2.recordDate) = 1
+where w1.temperature > w2.temperature;
+

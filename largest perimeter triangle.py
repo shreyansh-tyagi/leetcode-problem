a634c370fe76631649441c898b8cca8723b30207
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-07-21 17:17:37 +0530

    largest perimeter triangle

---
+++ largest perimeter triangle.py
@@ -0,0 +1,37 @@
+'''
+Given an integer array nums, return the largest perimeter of a triangle with a non-zero area, formed from three of these lengths. If it is impossible to form any triangle of a non-zero area, return 0.
+
+ 
+
+Example 1:
+
+Input: nums = [2,1,2]
+Output: 5
+Example 2:
+
+Input: nums = [1,2,1]
+Output: 0
+Example 3:
+
+Input: nums = [3,2,3,4]
+Output: 10
+Example 4:
+
+Input: nums = [3,6,2,3]
+Output: 8
+ 
+
+Constraints:
+
+3 <= nums.length <= 104
+1 <= nums[i] <= 106
+
+'''
+
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        for i in range(len(nums)-2):
+            if nums[i]<nums[i+1]+nums[i+2]:
+                return nums[i]+nums[i+1]+nums[i+2]
+        return 0    

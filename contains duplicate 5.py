cdab16ac4f4c6ccd5488893732891094e6b66cc9
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-07-25 21:39:18 +0530

    contains duplicate

---
+++ contains duplicate 5.py
@@ -0,0 +1,41 @@
+'''
+Given an integer array nums and two integers k and t, return true if there are two distinct indices i and j in the array such that abs(nums[i] - nums[j]) <= t and abs(i - j) <= k.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3, t = 0
+Output: true
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1, t = 2
+Output: true
+Example 3:
+
+Input: nums = [1,5,9,1,5,9], k = 2, t = 3
+Output: false
+ 
+
+Constraints:
+
+0 <= nums.length <= 2 * 104
+-231 <= nums[i] <= 231 - 1
+0 <= k <= 104
+0 <= t <= 231 - 1
+
+
+'''
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
+        if t == 0 and len(nums) == len(set(nums)):
+            return False
+        
+        for i in range(0, len(nums)):
+            for j in range(1, len(nums)):
+                if i == j:
+                    continue
+                elif abs(nums[j]-nums[i]) <= t and abs(j-i) <= k:
+                    return True
+
+        return False

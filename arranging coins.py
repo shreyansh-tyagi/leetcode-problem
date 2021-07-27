c661cdba2616743285afbb5690b7d8ecb9b8da9b
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-07-27 18:59:42 +0530

    arranging coins

---
+++ arranging coins.py
@@ -0,0 +1,37 @@
+'''
+You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
+
+Given the integer n, return the number of complete rows of the staircase you will build.
+
+ 
+
+Example 1:
+
+
+Input: n = 5
+Output: 2
+Explanation: Because the 3rd row is incomplete, we return 2.
+Example 2:
+
+
+Input: n = 8
+Output: 3
+Explanation: Because the 4th row is incomplete, we return 3.
+ 
+
+Constraints:
+
+1 <= n <= 231 - 1
+
+
+'''
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        a=0
+        while n>0:
+            a+=1
+            n-=a
+        if n==0:
+            return a
+        if n<0:
+            return a-1       

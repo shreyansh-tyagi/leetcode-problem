b038e3daa10fe6f378c9080bcbee8842a02d37c0
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-08-09 23:38:37 +0530

    sort linked list

---
+++ sort linked list.py
@@ -0,0 +1,47 @@
+'''
+Given the head of a linked list, return the list after sorting it in ascending order.
+
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+
+ 
+
+Example 1:
+
+
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+Example 2:
+
+
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 5 * 104].
+-105 <= Node.val <= 105
+
+
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        a,b=head,[]
+        while a:
+            b.append(a.val)
+            a=a.next
+        b.sort()
+        a=head
+        for i in b:
+            a.val,a=i,a.next
+        return head    
+            

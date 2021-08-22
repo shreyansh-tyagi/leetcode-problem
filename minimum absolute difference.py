afe4f5928efddaacef564aebfbb97161891b9304
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-08-22 17:37:02 +0530

    minimum absolute difference

---
+++ minimum absolute difference.py
@@ -0,0 +1,48 @@
+'''
+Given an array of distinct integers arr, find all pairs of elements with the minimum absolute difference of any two elements. 
+
+Return a list of pairs in ascending order(with respect to pairs), each pair [a, b] follows
+
+a, b are from arr
+a < b
+b - a equals to the minimum absolute difference of any two elements in arr
+ 
+
+Example 1:
+
+Input: arr = [4,2,1,3]
+Output: [[1,2],[2,3],[3,4]]
+Explanation: The minimum absolute difference is 1. List all pairs with difference equal to 1 in ascending order.
+Example 2:
+
+Input: arr = [1,3,6,10,15]
+Output: [[1,3]]
+Example 3:
+
+Input: arr = [3,8,-10,23,19,-4,-14,27]
+Output: [[-14,-10],[19,23],[23,27]]
+ 
+
+Constraints:
+
+2 <= arr.length <= 10^5
+-10^6 <= arr[i] <= 10^6
+
+
+'''
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        a,b,e=[],0,[]
+        arr.sort()
+        c=arr[1]-arr[0]
+        for i in range(1,len(arr)):
+            if arr[i]-arr[b]==c:
+                a.append(arr[b])
+                a.append(arr[i])
+            b+=1
+            if len(a)>1:
+                d=a.copy()
+                e.append(d)
+                a.clear()
+        return e    
+            

e5208eb709f48009625256c9ad4b93f51b01b70d
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-08-31 01:37:28 +0530

    find the common chracter

---
+++ find common character.py
@@ -0,0 +1,35 @@
+'''
+Given a string array words, return an array of all characters that show up in all strings within the words (including duplicates). You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: words = ["bella","label","roller"]
+Output: ["e","l","l"]
+Example 2:
+
+Input: words = ["cool","lock","cook"]
+Output: ["c","o"]
+ 
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 100
+words[i] consists of lowercase English letters.
+
+'''
+from collections import Counter
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        a=[]
+        for i in words:
+            a.append(Counter(i))
+        b,c=a[0],[]
+        for i in a:
+            b=b&i
+        for key,value in b.items():
+            for i in range(value):
+                c.append(key)
+        return c        

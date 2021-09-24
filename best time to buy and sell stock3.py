0b38778c060be86f99950b2a9e6ecdeb5b1ee752
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-09-24 23:43:22 +0530

    best time to sell and buy the good

---
+++ best time to buy and sell stock3.py
@@ -0,0 +1,41 @@
+'''
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day, and an integer k.
+
+Find the maximum profit you can achieve. You may complete at most k transactions.
+
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+ 
+
+Example 1:
+
+Input: k = 2, prices = [2,4,1]
+Output: 2
+Explanation: Buy on day 1 (price = 2) and sell on day 2 (price = 4), profit = 4-2 = 2.
+Example 2:
+
+Input: k = 2, prices = [3,2,6,5,0,3]
+Output: 7
+Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4. Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+ 
+
+Constraints:
+
+0 <= k <= 100
+0 <= prices.length <= 1000
+0 <= prices[i] <= 1000
+
+'''
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        if not k or not prices: return 0
+        if k >= len(prices)/2:
+            return sum(i - j for i, j in zip(prices[1:], prices[:-1]) if i - j > 0)
+        release=[0]*k
+        hold = [float("-inf")]*k
+        for price in prices:
+            for i in range (k-1, -1, -1):
+                release[i] = max(release[i], hold[i]+price)
+                if i==0: hold[i] = max(hold[i], -price)
+                else: hold[i] = max(hold[i], release[i-1]-price)
+        return release[-1]       

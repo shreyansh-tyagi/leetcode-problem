3622079710e4cfc7ac6fd774683319efdf7d464d
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-09-28 13:57:49 +0530

    reoder list

---
+++ reoder list.py
@@ -0,0 +1,50 @@
+'''
+You are given the head of a singly linked-list. The list can be represented as:
+
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+Example 2:
+
+
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 5 * 104].
+
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        a=[]
+        b=head
+        while b:
+            a.append(b.val)
+            b=b.next
+        b=head
+        for i in range(len(a)//2):
+            b.val,b=a[i],b.next
+            b.val,b=a[len(a)-i-1],b.next
+        if len(a)%2==1:
+            b.val=a[len(a)//2]
+             

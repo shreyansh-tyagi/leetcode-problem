6b4838a32ad70a63a58eee11d4e6e9579ef6bb65
Shreyansh Tyagi <sunnytyagi886@gmail.com>
2021-10-01 23:59:08 +0530

    remove outermost paranthesis

---
+++ remove outermost paranthesis.py
@@ -0,0 +1,56 @@
+'''
+A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+
+For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B nonempty valid parentheses strings.
+
+Given a valid parentheses string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses strings.
+
+Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
+
+ 
+
+Example 1:
+
+Input: s = "(()())(())"
+Output: "()()()"
+Explanation: 
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+Example 2:
+
+Input: s = "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation: 
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+Example 3:
+
+Input: s = "()()"
+Output: ""
+Explanation: 
+The input string is "()()", with primitive decomposition "()" + "()".
+After removing outer parentheses of each part, this is "" + "" = "".
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is either '(' or ')'.
+s is a valid parentheses string.
+'''
+
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        d = 0 # depth
+        r = ""
+        for c in s:
+            if c == "(":
+                d += 1 # When we get a "(" just increase the depth
+                if d > 1: # If the depth is > 1 now, meaning that it's nested, we need to add it to the result; or it must be a outmost one
+                    r += "("
+            else:
+                if d > 1: # Accordingly we add it to the result and decrease the depth if needed
+                    r += ")"
+                d -= 1
+        return r
